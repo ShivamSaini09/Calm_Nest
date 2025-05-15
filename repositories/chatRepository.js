010121e3656892065e65eec23523f0de489d22b9
ShivamSaini09 <sainisaini82955@gmail.com>
2025-05-15 20:16:21 +1000

    initial backend push

--- repositories/chatRepository.js
+++
@@ -1,8 +0,0 @@
-const chat = require("../models/chatModel");
-
-const saveChat = async (answers, riskLevel) => {
-  const newChat = new Chat({ answers, riskLevel });
-  return await newChat.save();
-};
-
-module.exports = { saveChat };
