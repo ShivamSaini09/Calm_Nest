0ebf8cba7cae754a07d83732123da8851afc17ae
ShivamSaini09 <sainisaini82955@gmail.com>
2025-05-26 01:24:46 +1000

    Render 3.0

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 
+PYTHON_VERSION=${PYTHON_VERSION:-3.10.8}
+PYTHON_DIR="/opt/render/project/.venv"
+
+echo "Creating Python virtual environment..."
+python3 -m venv $PYTHON_DIR
+source $PYTHON_DIR/bin/activate
+
+echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
