3dcd227836614f6db0b9ea7dc885f801557cc7ec
ShivamSaini09 <sainisaini82955@gmail.com>
2025-05-26 01:01:39 +1000

    Updsate Render

--- setup.sh
+++ setup.sh
@@ -1,15 +1,3 @@
 
-PYTHON_VERSION=${PYTHON_VERSION:-3.10.8}
-PYTHON_DIR="/opt/render/project/.venv"
-
-if [ ! -d "$PYTHON_DIR" ]; then
-  echo "Installing Python $PYTHON_VERSION..."
-  curl -sSL https://install.python-poetry.org | python3 -
-  sudo apt-get update
-  sudo apt-get install -y python3-pip python3-venv
-fi
-
-python3 -m venv $PYTHON_DIR
-source $PYTHON_DIR/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
