52ad15b61406ab8831d09a7cd394f632140d25b6
ShivamSaini09 <sainisaini82955@gmail.com>
2025-05-26 13:39:14 +1000

    render

--- setup.sh
+++ setup.sh
@@ -1,14 +1,3 @@
-PYTHON_VERSION=${PYTHON_VERSION:-3.11.11}
-PYTHON_DIR="/opt/render/project/.venv"
-
-echo "Creating Python virtual environment..."
-python3 -m venv $PYTHON_DIR
-source $PYTHON_DIR/bin/activate
-
-echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Ensuring venv Python is used by default..."
-echo "export PATH=\"$PYTHON_DIR/bin:\$PATH\"" >> ~/.bashrc
-source ~/.bashrc
+echo "Installing Python dependencies..."
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
